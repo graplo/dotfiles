ed5a980c3c486f0d751bc84297eec2cd52b9f792
Graplo <graplo@gmail.com>
2026-03-10 22:05:27 -0500

    Actualización de la conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,23 +1,21 @@
-# ~ / .config / fish / config.fish
+source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# Rutas de binarios (Equivalente a lo que tenías en .bashrc)
-# fish_add_path es inteligente y no duplica rutas si ya existen
-fish_add_path $HOME/.local/bin $HOME/bin $HOME/.cargo/bin
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+# Rutas
+fish_add_path $HOME/.local/bin $HOME/.cargo/bin
 
 if status is-interactive
-    # 1. Aplicar el tema Catppuccin Mocha
     fish_config theme choose Dracula
-
-    # 2. Inicializar Zoxide
     zoxide init fish | source
-
-    # 3. Integración de Ghostty (mejorada)
     if set -q GHOSTTY_RESOURCES_DIR
         source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
     end
 end
 
-# 4. Función Yazi (Tu función para cambiar de directorio al salir)
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     command yazi $argv --cwd-file="$tmp"
@@ -27,5 +25,20 @@ function y
     rm -f -- "$tmp"
 end
 
-# para iniciar starship con los colores dracula
 starship init fish | source
+alias hx='helix'
+
+set -gx QT_QPA_PLATFORMTHEME gtk3
+
+set -x EDITOR helix
+set -x VISUAL helix
+
+# Servicio de busqueda q usa torrra para descargar torrents
+alias jack="sudo systemctl start jackett"
+alias jackoff="sudo systemctl stop jackett"
+
+# para q torra se ejecute con rrr
+alias rrr="torrra"
+
+# Para hledger le asigno hl
+alias hl="hledger"
